7ea0d3026b898465f91d5a6538ba95534bdfc9bd
pcoolk <prashantkaushal996@gmail.com>
2025-07-25 23:03:05 +0530

    added courses page using bread-crumb. added config package with webConfig to be used as a configuration class for courses, about, etc views.

---
+++ src/main/java/com/prashant/school/schoolapp/config/webConfig.java
@@ -0,0 +1,15 @@
+package com.prashant.school.schoolapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class webConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/courses").setViewName("courses");
+    }
+
+}
