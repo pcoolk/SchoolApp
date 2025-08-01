124b9870da5f7cf26d9bbdbbfaf206f970130219
pcoolk <prashantkaushal996@gmail.com>
2025-08-01 22:50:46 +0530

    Configured multiple users in SecurityConfig using deprecated withDefaultPasswordEncoder

--- src/main/java/com/prashant/school/schoolapp/config/SecurityConfig.java
+++ src/main/java/com/prashant/school/schoolapp/config/SecurityConfig.java
@@ -4,14 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilter(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/","/home","/holiday","/saveMsg","/assets/**").permitAll()
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/holiday","/saveMsg","/assets/**").permitAll()
+                        .requestMatchers("/","/home").authenticated()
                         .requestMatchers("/contact").authenticated()
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
@@ -19,4 +24,20 @@ public class SecurityConfig {
 
         return http.build();
     }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username(("admin"))
+                .password("12345")
+                .roles("user")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("1234")
+                .roles("admin")
+                .build();
+    return new InMemoryUserDetailsManager(user, admin);
+    }
+
 }
