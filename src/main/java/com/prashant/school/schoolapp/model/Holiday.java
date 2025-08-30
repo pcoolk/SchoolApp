7c191a978b0121d0f25b7becfc2f96c740ef9f99
pcoolk <prashantkaushal996@gmail.com>
2025-08-30 16:21:01 +0530

    added JPA dependency, configured model classes

--- src/main/java/com/prashant/school/schoolapp/model/Holiday.java
+++ src/main/java/com/prashant/school/schoolapp/model/Holiday.java
@@ -1,12 +1,18 @@
 package com.prashant.school.schoolapp.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 //@Data is the annotation provided by lombok.
 @Data
+@Entity
+@Table(name = "holidays")
 public class Holiday extends BaseEntity{
+    @Id
     private  String day;
     private  String reason;
+
+    @Enumerated(EnumType.STRING)
     private  Type type;
 
     public enum Type{
